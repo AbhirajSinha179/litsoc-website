d78ccccd40d0e4eb2d2bbcfcdf35f5ec96cf74b6
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-05-23 12:17:55 +0530

    updated path

--- components/LitsocLogo.tsx
+++ components/LitsocLogo.tsx
@@ -1,4 +1,4 @@
-import LitsocIcon from '/assets/images/logo_litsoc_wh.png'
+import LitsocIcon from '@/public/assets/images/logo_litsoc_wh.png'
 import Image from 'next/image'
 
 export function LitsocLogo() {
