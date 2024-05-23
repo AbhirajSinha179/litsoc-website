a70466d44bee790af412985ef7102a11d735ec4f
agrpranjal07 <humesh2258@gmail.com>
2024-05-23 21:20:32 +0530

    .env updated with supabase

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,12 +17,7 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-      </div>
-
+    <div>
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
