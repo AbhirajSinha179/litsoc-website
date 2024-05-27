f7e8a66ae1c773c87679c6bebb63303468871320
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-05-27 09:19:34 +0530

    bro please be gtg now atleast

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,5 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
+
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
@@ -20,7 +18,7 @@ export default async function ProtectedPage() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center ">
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3 ">
-        <Header />
+        {/* <Header /> */}
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
