2b76f43e57dd3480ae8cf39b1d2a0698a07d170e
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-05-28 13:26:30 +0530

    footer added

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: {
@@ -7,9 +8,7 @@ export default function Layout({ children }: {
     <section className="flex flex-col items-center gap-20 w-full text-[#543310] bg-[#F8F4E1] min-h-screen">
       <Navbar />
       {children}
-      <footer id="contact" className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>The Literary Society, BIT Mesra</p>
-      </footer>
+      <Footer/>
     </section>
   );
 }
