a0b1eb633906364c7382a651c476c7b26a4703bd
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-05-28 23:36:02 +0530

    made fixes, removed footer images

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,8 +5,8 @@ export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3 ">
-        <div className="w-full flex flex-col gap-16 items-center">
-          <p className="text-2xl sm:text-4xl md:text-6xl lg:text-7xl !leading-tight mx-auto sm:text-center">
+        <div className="w-full flex flex-col gap-4 sm:gap-16 items-center">
+          <p className="text-4xl md:text-6xl lg:text-7xl !leading-tight mx-auto text-center">
             The Literary Society <br />{" "}
             <span className="opacity-80">BIT Mesra</span>
           </p>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,52 @@
-import Link from "next/link"
-import Image from "next/image"
-import Instagram from "@/public/images/instagram.png"
-import LinkedIn from "@/public/images/linkedIn.png"
-import Facebook from "@/public/images/facebook.png"
+import Link from "next/link";
+import Image from "next/image";
+import Instagram from "@/public/images/instagram.png";
+import LinkedIn from "@/public/images/linkedIn.png";
+import Facebook from "@/public/images/facebook.png";
 
 export default function Footer() {
   return (
     <footer id="contact" className="w-full px-10">
-        <div className='bg-[#543310] h-0.5 w-full rounded'></div>
-        <div className="w-full border-t border-t-foreground/10 md:px-4 px-0 pb-6 p-2 flex justify-between h-full text-center text-xs">
-            <p className=" md:hidden text-base flex justify-center mt-8  ">&#169; The Literary Society,<br/>BIT Mesra</p>
-            <p className="hidden md:flex text-base">&#169; The Literary Society,BIT Mesra</p>
-            <div className="flex justify-end flex-col md:flex-row">
-                <Link  href={"https://www.facebook.com/LiterarySocietyBITMesra/"} target="_blank" rel="noopener noreferrer"><Image src={Facebook} alt="" className="w-8 h-8 p-0.5 mx-2"/></Link>
-                <div className='border-r hidden md:block border-gray-300 h-8'></div>
-                <Link  href={"https://www.instagram.com/litsocbitm"} target="_blank" rel="noopener noreferrer"><Image src={Instagram} alt="" className="w-8 h-8 p-0.5 mx-2 my-2 md:my-0"/></Link>
-                <div className='border-r hidden md:block border-gray-300 h-8'></div>
-                <Link  href={"https://www.linkedin.com/company/literary-society-bit-mesra"} target="_blank" rel="noopener noreferrer"><Image src={LinkedIn} alt="" className="w-8 h-8 p-0.5 mx-2"/></Link>
-            </div>
+      <div className="bg-[#543310] h-0.5 w-full rounded"></div>
+      <div className="w-full border-t border-t-foreground/10 md:px-4 px-0 pb-6 p-2 flex justify-between h-full text-center text-xs">
+        <p className=" md:hidden text-base flex justify-center mt-8  ">
+          &#169; The Literary Society,
+          <br />
+          BIT Mesra
+        </p>
+        <p className="hidden md:flex text-base">
+          &#169; The Literary Society, BIT Mesra
+        </p>
+        <div className="flex justify-end flex-col md:flex-row">
+          <Link
+            href={"https://www.facebook.com/LiterarySocietyBITMesra/"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {/* <Image src={Facebook} alt="" className="w-8 h-8 p-0.5 mx-2" /> */}
+          </Link>
+          <div className="border-r hidden md:block border-gray-300 h-6"></div>
+          <Link
+            href={"https://www.instagram.com/litsocbitm"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {/* <Image
+              src={Instagram}
+              alt=""
+              className="w-8 h-8 p-0.5 mx-2 my-2 md:my-0"
+            /> */}
+          </Link>
+          <div className="border-r hidden md:block border-gray-300 h-6"></div>
+          <Link
+            href={"https://www.linkedin.com/company/literary-society-bit-mesra"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {/* <Image src={LinkedIn} alt="" className="w-8 h-8 p-0.5 mx-2" /> */}
+          </Link>
         </div>
+      </div>
     </footer>
-  )
+  );
 }
-
