d0eaa63ac0ec2a2a63c96f08ff359dcccb58e54d
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-05-29 19:32:12 +0530

    footer mobile view updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,18 +5,13 @@ const Phone_Number="tel:+91-620-448-9339";
 
 export default function Footer() {
   return (
-    <footer id="contact" className="w-full md:px-10 px-2">
+    <footer id="contact" className="w-full sm:px-10 px-2">
       <div className="bg-[var(--custom-color)] h-0.5 max-w-full mx-4 rounded"></div>
-      <div className="w-full border-t border-foreground/10 md:px-4 px-0 pb-6 py-2 flex justify-between items-center text-center text-xs">
-        <p className="text-base md:hidden mt-4">
-          &#169; The Literary Society,
-          <br />
-          BIT Mesra
-        </p>
-        <p className="hidden md:flex text-base">
+      <div className="w-full border-t border-foreground/10 px-4 pb-6 py-2 flex  sm:flex-row flex-col  sm:justify-between justify-center items-center text-center text-xs">
+        <p className="flex text-base">
           &#169; The Literary Society, BIT Mesra
         </p>
-        <div className="grid grid-cols-2 md:flex md:flex-row gap-4 mt-4 mx-2 md:mt-0 md:mx-0">
+        <div className="flex flex-row sm:gap-4 gap-8  mt-4  sm:mt-0 ">
           <Link
             href="https://www.facebook.com/LiterarySocietyBITMesra/"
             target="_blank"
