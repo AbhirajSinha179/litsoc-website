3ed5c46a80e630b9c3add41e4068210bbf3f6152
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-05-30 12:13:32 +0530

    feat: added pics and configured layout

--- app/(site)/team/page.tsx
+++ app/(site)/team/page.tsx
@@ -1,28 +1,29 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 async function getData() {
   // const res = await fetch("https://api.example.com/team");
   const res = {
     json: async () => [
       {
-        name: "Sparsh Anand",
-        role: "President",
-        image: "/team/1.jpg",
-      },
-      {
-        name: "Mohit Raj",
-        role: "Joint President",
-        image: "/team/2.jpg",
-      },
-      {
-        name: "Ayush",
-        role: "Joint President",
-        image: "/team/3.jpg",
-      },
-      {
         name: "Neeraj Kumar Singh",
         role: "Faculty Advisor",
-        image: "/team/4.jpg",
+        image: "/images/fac_ed.jpg",
+      },
+      {
+        name: "Lily",
+        role: "President",
+        image: "/images/prez_pic.jpg",
+      },
+      {
+        name: "Srishti Choudhary",
+        role: "Joint President",
+        image: "/images/joint_prez_1_pic.jpg",
+      },
+      {
+        name: "Shreyash Zade",
+        role: "Joint President",
+        image: "/images/joint_prez_2_pic.jpg",
       },
     ],
   };
@@ -37,16 +38,22 @@ export default async function Page() {
     <main className="flex flex-col gap-20 items-center">
       <Header title="Our Team" />
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-4  max-w-4xl gap-16">
+      <div className="grid sm:grid-cols-2 max-w-7xl gap-16">
         {data.map((member) => (
           <div key={member.name} className="flex flex-col gap-4 items-center">
-            <img src={member.image} alt={member.name} className="w-48 h-48" />
+            <Image
+              src={member.image}
+              alt={member.name}
+              className="rounded-md  "
+              width={400}
+              height={400}
+            />
             <div className="p-4 space-y-4 mx-auto text-center">
-              <p className="whitespace-nowrap">{member.name}</p>
-              <p>{member.role}</p>
+              <p className="font-bold  text-xl">{member.role}</p>
+              <p className="whitespace-nowrap text-lg">{member.name}</p>
             </div>
           </div>
-        ))} 
+        ))}
         {/* <div className="flex flex-col gap-4 items-center">
           <img src="/team/1.jpg" alt="Team member 1" className="w-48 h-48" />
           <div className="p-4 space-y-4 mx-auto text-center">
@@ -56,11 +63,18 @@ export default async function Page() {
         </div> */}
       </div>
 
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 "> k20s </h2>
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 "> k21s </h2>
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 "> k22s </h2>
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 "> k23s </h2>
-
+      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 ">
+        {" "}
+        k21s{" "}
+      </h2>
+      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 ">
+        {" "}
+        k22s{" "}
+      </h2>
+      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl border-b border-b-black/20 ">
+        {" "}
+        k23s{" "}
+      </h2>
     </main>
   );
 }
