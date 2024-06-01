eb801c2b05d1865225d7ab8d27a14187427a0020
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-06-02 02:45:23 +0530

    fix: build fixes

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_ANON_KEY as string);
 
 export async function getWorkById(id: string) {
   const { data, error }  = await supabase.from("works").select().eq("id", id);
