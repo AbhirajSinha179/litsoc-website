d43cac2e730e6e1eb00fe516c4810f13460c940e
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-06-02 02:56:15 +0530

    fix: temp build env changes

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,8 +2,8 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
 // export const supabase = createClient();
