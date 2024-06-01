cd53acda924d3d98e6789482c498de446c13ea51
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-06-02 02:38:19 +0530

    added in work headers as well

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { gilda } from "@/utils/fonts";
 export default function Header({title}: {title: string}) {
   return (
     <div className="animate-in flex flex-col gap-8 items-center w-full">
-      <p className="text-4xl md:text-6xl lg:text-7xl !leading-tight mx-auto sm:text-center" style={gilda.style}>
+      <p className="text-4xl sm:text-6xl lg:text-7xl !leading-tight mx-auto sm:text-center" style={gilda.style}>
         {title}
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-[--custom-color] to-transparent my-8" />
