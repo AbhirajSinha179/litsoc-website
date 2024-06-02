7c272acfc613b5412fd3a31001deb5288ae9915c
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-06-02 19:13:51 +0530

    updated encvironment variables

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
 
 export async function getWorkById(id: number) {
   const { data, error }  = await supabase.from("works").select().eq("id", id);
