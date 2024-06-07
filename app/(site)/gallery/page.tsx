54b003d1d9efda87359a5caa7a8ba18ca08756e2
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-06-08 01:42:43 +0530

    "Added Litnight and Rostra images to Gallery page, updated Carousel component with autoplay feature, and modified getGalleryImages function to fetch images from multiple folders."

--- app/(site)/gallery/page.tsx
+++ app/(site)/gallery/page.tsx
@@ -1,12 +1,15 @@
 
 import Header from "@/components/Header";
 import Carousel from "@/components/Carousel"
+import GalleryImage from "@/components/GalleryImage";
+
 export default async function Gallery() {
  
     return (
       <main className="flex flex-col gap-20 items-center">
       <Header title="Gallery" />
       <Carousel/>
+      <GalleryImage/>
       </main>
     );
   }
