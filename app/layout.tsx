000567e72c9275114a308d48fe44f752c351f681
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-07-05 23:03:46 +0530

    "Update metadata and add HTML head tags to RootLayout component"

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "The Literary Society",
-  description: "The official website of The Literary Society, BIT Mesra",
+  title: "The Literary Society - BIT Mesra",
+  description: "Welcome to the official website of The Literary Society at BIT Mesra, showcasing our activities and events.",
 };
 
 export default function RootLayout({
@@ -19,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+      </head>
+
       <body className="bg-background text-foreground ">
         <main className="min-h-screen flex flex-col items-center antialiased scroll-smooth">
           {children}
